3566e231e18fd502fa5662e4b2b9c9c04dcc3090
Daiqua <yoann.c.maillot@gmail.com>
2022-01-26 12:50:35 +0100

    repository test finished

--- todoc-master/app/src/test/java/com/cleanup/todoc/ProjectDataRepositoryTest.java
+++ todoc-master/app/src/test/java/com/cleanup/todoc/ProjectDataRepositoryTest.java
@@ -1,9 +1,9 @@
 package com.cleanup.todoc;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -15,14 +15,12 @@ import com.cleanup.todoc.database.dao.ProjectDao;
 import com.cleanup.todoc.model.Project;
 import com.cleanup.todoc.repositories.ProjectDataRepository;
 
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -39,12 +37,13 @@ public class ProjectDataRepositoryTest {
     @InjectMocks
     ProjectDataRepository projectDataRepository;
 
-    private List<Project> dummyProjectsList = Arrays.asList(
-            new Project("test1", 0),
-            new Project("test2", 1)
-    );
-    private MutableLiveData<List<Project>> mutableDummyProjectsList = new MutableLiveData<>();
-    private MutableLiveData<List<Project>> mutableDummyProjectsList2 = new MutableLiveData<>();
+    private final Project project1 = new Project("project1", 0); //dao id is 0
+    private final Project project2 = new Project("project2", 1);// dao id is 1
+    private final Project project3 = new Project("project3", 2);// dao id is 2
+
+    private List<Project> dummyProjectsList = Arrays.asList(project1, project2);
+
+    private final MutableLiveData<List<Project>> mutableDummyProjectsList = new MutableLiveData<>();
 
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
@@ -63,10 +62,38 @@ public class ProjectDataRepositoryTest {
         //load live project form repository
         final LiveData<List<Project>> liveProjects = projectDataRepository.getAllProjects();
         //checks
-        verify(projectDao).getAllProjects();
+        verify(projectDao, times(1)).getAllProjects();
         assertEquals(liveProjects.getValue().size(), dummyProjectsList.size());
         assertEquals(liveProjects.getValue().get(0), dummyProjectsList.get(0));
         assertFalse(liveProjects.getValue().get(0).equals(dummyProjectsList.get(1)));
 
     }
+
+    @Test
+    public void getProjectShouldReturnTheChosenProject() {
+        //Mock instruction
+        when(projectDao.getProject(0)).thenReturn(dummyProjectsList.get(0));
+        when(projectDao.getProject(1)).thenReturn(dummyProjectsList.get(1));
+        //get projects form repository
+        Project checkProject1 = projectDataRepository.getProject(0);
+        assertEquals(checkProject1, project1);
+        Project checkProject2 = projectDataRepository.getProject(1);
+        assertEquals(checkProject2, project2);
+        verify(projectDao, times(2)).getProject(any(Long.class));
+        assertFalse(checkProject2.equals(project1));
+    }
+
+    @Test
+    public void insertProjectShouldAddOneProjectInTheProjectListFromDao() {
+        //TODO check with Brahim - is verify enough for void method?
+        //Mock instruction
+        //add the project
+        projectDataRepository.insertProject(project3);
+        //checks
+        verify(projectDao, times(1)).insertProject(any(Project.class));
+        //assertEquals(mutableDummyProjectsList.getValue().size(), 3); TODO: remove
+        //assertEquals(mutableDummyProjectsList.getValue().get(3), project3); TODO: remove
+
+    }
+
 }
--- todoc-master/app/src/test/java/com/cleanup/todoc/TaskDataRepositoryTest.java
+++ todoc-master/app/src/test/java/com/cleanup/todoc/TaskDataRepositoryTest.java
@@ -1,4 +1,101 @@
 package com.cleanup.todoc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.cleanup.todoc.database.dao.TaskDao;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+import com.cleanup.todoc.repositories.TaskDataRepository;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class TaskDataRepositoryTest {
+
+    //dummy task DAO
+    @Mock
+    TaskDao taskDao;
+
+    @InjectMocks
+    TaskDataRepository taskDataRepository;
+
+    private final Task task1 = new Task(0, "task1", 0);
+    private final Task task2 = new Task(1, "task1", 1);
+    private final Task task3 = new Task(2, "task1", 2);
+
+    private List<Task> dummyTasksList = Arrays.asList(task1, task2);
+
+    private final MutableLiveData<List<Task>> mutableDummyTasksList = new MutableLiveData<>();
+
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        taskDataRepository = new TaskDataRepository(taskDao);
+        mutableDummyTasksList.setValue(dummyTasksList);
+    }
+
+    @Test
+    public void getAllProjectsShouldReturnAllProjects() {
+        //Mock instruction
+        when(taskDao.getAllTasks()).thenReturn(mutableDummyTasksList);
+        //load live project form repository
+        final LiveData<List<Task>> liveTasks = taskDataRepository.getAllTasks();
+        //checks
+        verify(taskDao, times(1)).getAllTasks();
+        assertEquals(liveTasks.getValue().size(), dummyTasksList.size());
+        assertEquals(liveTasks.getValue().get(0), dummyTasksList.get(0));
+        assertFalse(liveTasks.getValue().get(0).equals(dummyTasksList.get(1)));
+    }
+
+    @Test
+    public void insertTaskShouldAddOneTaskFromDao() {
+        //TODO check with Brahim - is verify enough for void method?
+        //Mock instruction
+        //add the task
+        taskDataRepository.insertTask(task3);
+        //checks
+        verify(taskDao, times(1)).insertTask(task3);
+        //assertEquals(mutableDummyTasksList.getValue().size(), 3); TODO: remove
+        //assertEquals(mutableDummyTasksList.getValue().get(3), task3); TODO: remove
+    }
+
+    @Test
+    public void deleteTaskShouldRemoveOneTaskTFromDao() {
+        //TODO check with Brahim - is verify enough for void method?
+        //Mock instruction
+        taskDataRepository.deleteTask(task2.getId());
+        //checks
+        verify(taskDao, times(1)).deleteTask(task2.getId());
+    }
 }
+
+
